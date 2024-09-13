63241eee2069fed2e965937de88ecf4e81c81c0f
Startom78 <tom78630@gmail.com>
2024-09-13 16:13:14 +0200

    initialized const to reach formular data

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,4 +20,21 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+function validateFormular() {
+  const first_name = document.getElementById(".first")
+  const last_name = document.getElementById(".last")
+  const email = document.getElementById(".email")
+  const birthdate = document.getElementById(".birthdate")
+  const number_tournament = document.getElementById(".quantity")
+}
+
+//TODO tests manuels
+//TODO Ajouter confirmation quand envoi réussi
+//TODO Ajouter validation ou message d'erreur
+//TODO Implémenter entrée du formulaire
+//TODO Fermer la modale
+
+
+
+
 
